e4cf1c368ace689eaee523aadfe9ae63921704ec
Zülfikar Ata <zulfikarata07@gmail.com>
2023-09-23 14:49:56 +0300

    Proje dosyası ekle.

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core.Entities
+{
+    public interface IEntity
+    {
+    }
+}
